78784782bb78f81200dd26f64952b3393effe92f
Sigfried Gold <sgold@s-3.com>
2014-01-03 09:39:06 -0500

    fixed problem with trying to enhance frozen objects. needs work. also selecting underscore methods better now.

--- src/underscore-unchained.js
+++ src/underscore-unchained.js
@@ -14,6 +14,7 @@ to mess with _.chain() and .value() every time you do two Underscore methods in 
     @param {String[]} [magicPowers.exclude] any Underscore functions you don't want.
     @param {Object} [magicPowers.more] map of any other methodNames and functions you'd like thrown in.
     @param {Object} [magicPowers.plainPrimitives=false] if true, don't turn primitive data types into Objects for enhancement.
+    @param {Object} [magicPowers.cloneFrozenVals=false] if true and return vals are unextensible, clone them for enhancement.
     @return {Object} enhanced with methods which all return similarly enhanced results
     @example ```
 _.unchain(['a','bb','ccc'])
@@ -31,17 +32,17 @@ _.unchain(['a','bb','ccc'],{plainPrimitives:true})
     */
     unchain: function(obj, magicPowers) {
         var map = {};
-        var unames = magicPowers && magicPowers.include ||
-            _.chain(_.prototype)
-                .keys()
-                .difference(Object.getOwnPropertyNames(Array.prototype))
-                .value();
+        var plainPrimitives = magicPowers && magicPowers.plainPrimitives;
+        var cloneFrozenVals = magicPowers && magicPowers.cloneFrozenVals;
+        var unames = magicPowers && magicPowers.include || 
+            Object.getOwnPropertyNames(_)
+                .filter(function(d) { return _.isFunction(_[d]) });
         _(unames).each(function(underscoreFuncName) {
             map[underscoreFuncName] = _[underscoreFuncName];
         });
         if (magicPowers && magicPowers.more)
             _.extend(map, magicPowers.more);
-        return enhance(obj, map, magicPowers && magicPowers.plainPrimitives);
+        return enhance(obj, map, plainPrimitives, cloneFrozenVals);
     },
 /*
 ## _.prometheus(obj, magicPowers)
@@ -63,21 +64,39 @@ _.prometheus(Array);
     }
 });
 
-function enhance(obj, funcsAndNames, plainPrimitives) {
-    _.chain(funcsAndNames).pairs().each(function(pair) {
-        var methodName = pair[0], func = pair[1];
-        Object.defineProperty(obj, methodName, {
-            value: function() {
-                var result = func.apply(_, [this].concat(_.toArray(arguments)));
-                if (result instanceof Object)
-                    return enhance(result, funcsAndNames, plainPrimitives);
-                if (plainPrimitives)
-                    return result;
-                return enhance(new Object(result), funcsAndNames, plainPrimitives);
-            }
+function enhance(obj, funcsAndNames, plainPrimitives, cloneFrozenVals) {
+    try {
+        _.chain(funcsAndNames).pairs().each(function(pair) {
+            var methodName = pair[0], func = pair[1];
+            Object.defineProperty(obj, methodName, {
+                value: function() {
+                    try {
+                        var result = func.apply(_, [this].concat(_.toArray(arguments)));
+                    } catch(e) {
+                        console.log("failed to apply " + methodName);
+                        console.log(e.stack);
+                        throw e;
+                    }
+                    if (result instanceof Object)
+                        return enhance(result, funcsAndNames, plainPrimitives, cloneFrozenVals);
+                    if (plainPrimitives)
+                        return result;
+                    return enhance(new Object(result), funcsAndNames, plainPrimitives, cloneFrozenVals);
+                }
+            });
         });
-    });
-    return obj;
+    }
+    catch(e) {
+        if (e.message.substr(-25) === "object is not extensible." && cloneFrozenVals) {
+            obj = _.clone(obj);
+            enhance(obj, funcsAndNames, plainPrimitives, cloneFrozenVals);
+        } else {
+            console.log(e.stack);
+        }
+    }
+    finally {
+        return obj;
+    }
 }
 if (typeof exports !== 'undefined') {   // not sure if this is all right
     if (typeof module !== 'undefined' && module.exports) {
